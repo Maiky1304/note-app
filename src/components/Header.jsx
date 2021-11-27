8e1d5d2f906798c445b1fcd8034aeba5d383ecd6
maiky perlee <maikyperlee@icloud.com>
2021-11-27 21:24:59 +0100

    started

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import './Header.css';
+
+function Header() {
+    return (
+        <div className="header">
+            <h1 className="header_text">Header</h1>
+            <button className="header_btn">Toggle theme</button>
+        </div>
+    )
+}
+
+export default Header;
